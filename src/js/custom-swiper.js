4e3700eb206b9656154ce267ca3a17f520be9c52
noleemits <stevenoleemits@gmail.com>
2023-02-06 12:10:33 -0600

    Added scripts for swiper and deleted the branch where this was practiced

---
+++ src/js/custom-swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.mySwiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
